445799016035f7dc70da35477cb1ca07ba005088
Feret Thomas <thomas.feret@hotmail.fr>
2026-07-08 16:00:21 +0200

    Saut avec detection sol et condition Idle corrigee

--- godot_project/scripts/Personnage.cs
+++ godot_project/scripts/Personnage.cs
@@ -2,62 +2,70 @@ using Godot;
 
 public partial class Personnage : CharacterBody3D
 {
-	[Export] public float VitesseMarche = 3.0f;
-	[Export] public float VitesseCourse = 6.0f;
-	[Export] public float Gravite = 9.8f;
+		[Export] public float VitesseMarche = 3.0f;
+		[Export] public float VitesseCourse = 6.0f;
+		[Export] public float Gravite = 9.8f;
+		[Export] public float ForceSaut = 5.0f;
 
-	private AnimationTree _animTree;
-	private AnimationNodeStateMachinePlayback _sm;
-	private string _etatCourant = "";
+		private AnimationTree _animTree;
+		private AnimationNodeStateMachinePlayback _sm;
+		private string _etatCourant = "";
 
-	public override void _Ready()
-	{
-		_animTree = GetNode<AnimationTree>("ybot/AnimationTree");
-		_animTree.Active = true;
-		_sm = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
-		_ChangerEtat("Idle");
-	}
-
-	private void _ChangerEtat(string nouvelEtat)
-	{
-		if (_etatCourant == nouvelEtat) return;
-		_etatCourant = nouvelEtat;
-		_sm.Travel(nouvelEtat);
-	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
-
-		if (!IsOnFloor())
-			velocity.Y -= Gravite * (float)delta;
-
-		Vector2 input = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = new Vector3(input.X, 0, input.Y).Normalized();
-
-		bool courir = Input.IsActionPressed("ui_accept");
-
-		if (direction.Length() > 0.1f)
+		public override void _Ready()
 		{
-			float vitesse = courir ? VitesseCourse : VitesseMarche;
-			velocity.X = direction.X * vitesse;
-			velocity.Z = direction.Z * vitesse;
-
-			if (courir)
-				_ChangerEtat("Running");
-			else
-				_ChangerEtat("Walking");
-
-			LookAt(GlobalPosition + new Vector3(direction.X, 0, direction.Z), Vector3.Up);
-		}
-		else
-		{
-			velocity.X = 0;
-			velocity.Z = 0;
-			_ChangerEtat("Idle");
+				_animTree = GetNode<AnimationTree>("ybot/AnimationTree");
+				_animTree.Active = true;
+				_sm = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
+				_ChangerEtat("Idle");
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+		private void _ChangerEtat(string nouvelEtat)
+		{
+				if (_etatCourant == nouvelEtat) return;
+				_etatCourant = nouvelEtat;
+				_sm.Travel(nouvelEtat);
+		}
+
+		public override void _PhysicsProcess(double delta)
+		{
+				Vector3 velocity = Velocity;
+				if (!IsOnFloor())
+						velocity.Y -= Gravite * (float)delta;
+
+				// Saut
+				if (Input.IsActionJustPressed("sauter") && IsOnFloor())
+				{
+						velocity.Y = ForceSaut;
+						_ChangerEtat("Jump");
+				}
+				Vector2 input = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+				Vector3 direction = new Vector3(input.X, 0, input.Y).Normalized();
+
+				bool courir = Input.IsActionPressed("ui_accept");
+
+				if (direction.Length() > 0.1f)
+				{
+						float vitesse = courir ? VitesseCourse : VitesseMarche;
+						velocity.X = direction.X * vitesse;
+						velocity.Z = direction.Z * vitesse;
+
+						if (courir)
+								_ChangerEtat("Running");
+						else
+								_ChangerEtat("Walking");
+
+						LookAt(GlobalPosition + new Vector3(direction.X, 0, direction.Z), Vector3.Up);
+				}
+				else
+				{
+						velocity.X = 0;
+						velocity.Z = 0;
+
+						if (IsOnFloor())
+								_ChangerEtat("Idle");
+				}
+
+				Velocity = velocity;
+				MoveAndSlide();
+		}
 }
